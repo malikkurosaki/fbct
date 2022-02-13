bf4ec322fc1d0673131fb4aaed88d9a78f369b1d
malikkurosaki <kurosakiblackangel@gmail.com>
2022-02-13 19:01:05 +0800

    baru

--- xserver/server.js
+++ xserver/server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+app.use(cors());
+app.use(express.static('../build/web'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.listen(5000, () => console.log("server verjalan di port 5000"));
